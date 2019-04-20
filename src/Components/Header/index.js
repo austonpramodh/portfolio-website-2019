341a441d64311c518b532f82ba0bfab53380c29c
Auston Pramodh Barboza <austonpramodh@gmail.com>
2019-04-20 11:55:26 +0530

    anchor link

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Zoom } from "react-reveal";
 import injectSheet from "react-jss";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import Icon from "@mdi/react";
 import { mdiArrowDownThick } from "@mdi/js";
 import Styles from "./index.css";
 import Typer from "../Typer";
 import "./animations.css";
 
+// eslint-disable-next-line no-undef
 const isMobile = window.innerWidth < 700;
 
 const Header = props => {
@@ -33,8 +35,9 @@ const Header = props => {
           />
         </h1>
       </Zoom>
-      {/* </div> */}
-      <Icon path={mdiArrowDownThick} size={2} className={classes.icon} />
+      <AnchorLink href="#aboutPage">
+        <Icon path={mdiArrowDownThick} size={2} className={classes.icon} />
+      </AnchorLink>
     </div>
   );
 };
